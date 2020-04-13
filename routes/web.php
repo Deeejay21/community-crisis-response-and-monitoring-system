4e0dc3c76aa7dd69ce292b2327db581b81533281
david <bg1aa.cpe211.deleon160735@gmail.com>
2020-04-13 15:29:12 +0800

    Authentication package Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
 
-//Route::view('/', 'welcome')->name('site.index');
+Route::view('/', 'welcome')->name('site.index');
 // DAVID
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
